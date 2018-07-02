12f1c12cc05723d83cf35c3d96d4ba282c8dc39b
gauravjeet.singh <gauravjeet.singh@kochartech.com>
2018-07-02 15:14:13 +0530

    2nd test commit

--- app/src/main/java/com/gaurav/producttracker/MainActivity.java
+++ app/src/main/java/com/gaurav/producttracker/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: From here");
+        Log.d(TAG, "onCreate: From here");
     }
 }
